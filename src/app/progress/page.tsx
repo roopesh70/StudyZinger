9760d81ec8e48c0b549650cfc157a35fe12b4d34
Roopesh Krishnan <roopeshkrishnanme@gmail.com>
2025-09-24 17:27:51 +0000

    reduce the height  (_for element <CardContent>_)

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -319,7 +319,7 @@ export default function ProgressPage() {
                 <CardDescription>A look at your completed tasks month by month.</CardDescription>
             </CardHeader>
             <CardContent>
-                <ChartContainer config={monthlyChartConfig} className="min-h-[100px] w-full">
+                <ChartContainer config={monthlyChartConfig} className="min-h-[80px] w-full">
                 <BarChart accessibilityLayer data={monthlyChartData}>
                     <CartesianGrid vertical={false} />
                     <XAxis
@@ -343,6 +343,8 @@ export default function ProgressPage() {
     </main>
   );
 }
+
+    
 
     
 
